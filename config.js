3b96fcffe2c557918c163947cf336018aa0b0b8d
ayshajamjam <aj2604@columbia.edu>
2022-11-20 17:06:03 -0500

    Added tokens back

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Save API keys and access token info
 module.exports = {
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  '',
+    consumer_key:         'qS9W0hrctjLoVldpOabJox1Bb',
+    consumer_secret:      '8JtkpffXtdwqe5I7OGTUr5oBcSn61zrlUB302YuYLaGjIDLejv',
+    access_token:         '1593415433033584641-kpl4GJTFa7W8FuhYPnB3mTL7nHZ7oO',
+    access_token_secret:  'EQvna3tXNjImbJsBMumnxf9GNU818T1bMKE2QQhC54Mii',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
