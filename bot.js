68ec18fcc3e0657a9a60e0458bbdfff626e8f403
ayshajamjam <aj2604@columbia.edu>
2022-11-20 05:53:09 -0500

    Finished code. Need to deploy to Heroku

---
+++ bot.js
@@ -0,0 +1,98 @@
+// Reference: https://github.com/ttezel/twit
+// Making API requests with nodejs: https://hackersandslackers.com/making-api-requests-with-nodejs/
+
+var Twit = require('twit'); // Import Twit library
+var cronJob = require("cron").CronJob;
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+var request = require('request');
+var fs = require('fs');
+const request_promise = require('request-promise');
+
+let art_title = "";
+let art_artist = "";
+let art_date = "";
+let art_img = "";
+let art_type = "";
+
+// Chicago Arts Institute API stuff: no api key needed
+function getArtwork() {
+    let art_id = Math.floor(Math.random()*(117673 - 50 + 1) + 50);
+    const api_url = `https://api.artic.edu/api/v1/artworks/${art_id}?fields=id,title,artist_display,date_display,image_id,artwork_type_title`
+
+    const get_data = async api_url => {
+        try {
+            const response = await fetch(api_url);
+            const json = await response.json();
+            // console.log(json);
+            if(response.ok) {
+                art_title = json.data.title;
+                art_artist = json.data.artist_display;
+                art_date = json.data.date_display;
+                art_img = json.config.iiif_url + '/' + json.data.image_id + '/full/843,/0/default.jpg';
+                art_type = json.data.artwork_type_title;
+
+                // Download image from art_img (URL)
+                // https://stackoverflow.com/questions/12740659/downloading-images-with-node-js
+                var download = function (uri, filename, callback) {
+                    request.head(uri, function(err, res, body){
+                        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                    });
+                };
+                  
+                download(art_img, 'img.png', function(){
+                    tweet();
+                });
+            }
+            else {
+                // If it does't find a valid art piece then look for another one
+                getArtwork();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+      };
+      
+    get_data(api_url);
+}
+
+// Twitter stuff
+
+var config = require('./config');
+const { CronJob } = require('cron');
+var T = new Twit(config);
+
+// Post a new tweet
+function tweet() {
+    var rand = Math.floor(Math.random()*100);
+
+    new_tweet = 
+    `Title: ${art_title}, ${art_date}\nArtist: ${art_artist}\nType: ${art_type}`
+
+    var params = {encoding: 'base64'}
+
+    var b64 = fs.readFileSync('img.png', params);
+
+    T.post('media/upload', { media_data: b64 }, uploaded);
+
+    function uploaded(err, data, response){
+        var id = data.media_id_string;
+        var tweet = {
+            status: new_tweet,
+            media_ids: [id]
+        }
+        T.post('statuses/update', tweet, function(err, data, response) {
+            console.log(data.text)
+            console.log("Image Src: " + art_img);
+        })
+    }
+}
+
+// const job = new CronJob("* * * * *", () => {
+//     console.log("Cronjob started.");
+//     getArtwork();
+// })
+
+// job.start();
+
+getArtwork();
---
+++ config.js
@@ -0,0 +1,9 @@
+// Save API keys and access token info
+module.exports = {
+    consumer_key:         'QUgZxGyk8obvawGo0ZeEWbqDS',
+    consumer_secret:      'ajTBGzDLsenQyaZwdSsVMf0yVS5ibNMeEMcTiY8FDoKbEY4g3l',
+    access_token:         '1593415433033584641-AputeLlFWbgTTrbdHyQhSGyy7d6Pqa',
+    access_token_secret:  'TVOQdzsKuQhv7iW62HneAD2WzScAnKZxYMe1JDCurPlKk',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
